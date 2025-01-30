220e17c5c86315aed719baaa603002e24a9e02f7
Mireya Marín <mireya.marin@estudiants.urv.cat>
2025-01-30 18:03:43 +0100

    arreglo test

--- MyAdventures/test/test_emotionsBot.py
+++ MyAdventures/test/test_emotionsBot.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
-from agents.emotionsBot import EmotionBot
+from MyAdventures.agents.emotionsBot import EmotionBot
 
 class TestEmotionBot(unittest.TestCase):
     def setUp(self):
