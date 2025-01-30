0d6cb0967c619d0ad951334df920b07c5b5d9953
naymoon <154450493+naymoon@users.noreply.github.com>
2025-01-30 17:48:39 +0100

    Update OracleBot.py

--- MyAdventures/agents/OracleBot.py
+++ MyAdventures/agents/OracleBot.py
@@ -2,12 +2,13 @@ import time
 import mcpi.minecraft as minecraft
 from .AgentFramework import Agent
 
-class OracleBot(Agent):  # Hereda de Agent
+
+class OracleBot(Agent):
     def __init__(self, mc):
-        super().__init__("OracleBot")  # Llamada al constructor Agent
+        super().__init__("OracleBot")  # Call to Agent's constructor
         self.mc = mc
-        self.running = False 
-        
+        self.running = False
+
         self.responses = {
             "hello": "Hi there!",
             "how are you": "I'm doing well, thank you for asking.",
@@ -23,8 +24,11 @@ class OracleBot(Agent):  # Hereda de Agent
         self.mc.postToChat("OracleBot is online! Type 'bye' to stop me.")
         self.listen_for_questions()
 
-    def listen_for_questions(self):
-        while self.running:
+    def listen_for_questions(self, test_mode=False):
+
+        ######################################################
+        # para verificar q funcione en el test sin quedarse en el Loop
+        if test_mode:
             events = self.mc.events.pollChatPosts()
             for event in events:
                 question = event.message.lower()
@@ -32,9 +36,22 @@ class OracleBot(Agent):  # Hereda de Agent
                     self.mc.postToChat("Goodbye! OracleBot shutting down.")
                     self.stop()
                     return
-                response = self.responses.get(question, "I'm sorry, my programmer was too lazy to design a response to that kind of question ;)")
+                response = self.responses.get(question, "I'm sorry, my programmer was too lazy to design a response to that kind of question")
                 self.mc.postToChat(response)
-            time.sleep(1)
+        #######################################################
+        
+        else:
+            while self.running:
+                events = self.mc.events.pollChatPosts()
+                for event in events:
+                    question = event.message.lower()
+                    if question == "bye":
+                        self.mc.postToChat("Goodbye! OracleBot shutting down.")
+                        self.stop()
+                        return
+                    response = self.responses.get(question, "I'm sorry, my programmer was too lazy to design a response to that kind of question")
+                    self.mc.postToChat(response)
+                time.sleep(1)
 
     def stop(self):
         self.running = False
